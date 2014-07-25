13efec4da1fe60d5feeb7551ad316fc27b34d1f1
Michael Tibben <michael.tibben@99designs.com>
2014-07-26 02:08:30 +1000

    Initial commit for duh

---
+++ README.md
@@ -0,0 +1,12 @@
+# Duh
+
+A `du`-like tool that gathers file and directory size statistics and plots to a histogram in realtime.
+
+Install with `go install github.com/mtibben/duh`
+
+Run with `duh`
+
+TODO:
+ - interactive expandable tree
+ - colour
+ - globbing
---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/pivotal-golang/bytefmt"
+	"github.com/wsxiaoys/terminal"
+)
+
+type Node struct {
+	name     string
+	size     int64
+	children nodemap
+}
+
+func NewNode(name string) *Node {
+	return &Node{
+		name:     name,
+		children: nodemap{},
+	}
+}
+
+type nodes []*Node
+
+func (s nodes) Len() int      { return len(s) }
+func (s nodes) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+type BySize struct{ nodes }
+
+func (s BySize) Less(i, j int) bool { return s.nodes[i].size < s.nodes[j].size }
+
+type nodemap map[string]*Node
+
+func (mp nodemap) slice() nodes {
+	nodeslice := nodes{}
+	for _, n := range mp {
+		nodeslice = append(nodeslice, n)
+	}
+
+	return nodeslice
+}
+
+var rootnode *Node
+var lines = 0
+var files = 0
+
+func strhashsForPct(a, b int64) string {
+	t := math.Ceil((float64(a) / float64(b)) * 20)
+	return strings.Repeat("#", int(t))
+}
+
+func clearOutput() {
+	for i := 0; i < lines; i++ {
+		terminal.Stdout.Up(1)
+		terminal.Stdout.ClearLine()
+	}
+	lines = 0
+}
+func printNode(node *Node, tot int64) {
+	terminal.Stdout.Print(fmt.Sprintf("%21s %8s   %s\n", strhashsForPct(node.size, tot), bytefmt.ByteSize(uint64(node.size)), node.name))
+	lines++
+}
+func printTotals() {
+	terminal.Stdout.Print(fmt.Sprintf("%21s %8s   (%d files)\n", "TOTAL:", bytefmt.ByteSize(uint64(rootnode.size)), files))
+	lines++
+}
+func printHistogram(node *Node) {
+	nodeSlice := node.children.slice()
+	sort.Sort(BySize{nodeSlice})
+	largestSize := nodeSlice[len(nodeSlice)-1].size
+
+	clearOutput()
+	for _, n := range nodeSlice {
+		printNode(n, largestSize)
+	}
+	printTotals()
+}
+
+func addFile(path string, filesize int64) {
+	files++
+
+	relpath, _ := filepath.Rel(rootnode.name, path)
+	pathparts := strings.Split(relpath, string(filepath.Separator))
+
+	curnode := rootnode
+	curnode.size += filesize
+	for _, part := range pathparts {
+		childnode, exists := curnode.children[part]
+		if !exists {
+			childnode = NewNode(part)
+			curnode.children[part] = childnode
+		}
+		curnode = childnode
+		curnode.size += filesize
+	}
+}
+
+func addFileInfoToTree(path string, info os.FileInfo, err error) error {
+	if !info.IsDir() {
+		addFile(path, info.Size())
+	}
+
+	return nil
+}
+
+func isDir(dir string) bool {
+	fi, _ := os.Stat(dir)
+	return fi.IsDir()
+}
+
+func walkPathAndPrintResults(rootpath string) {
+	doneChan := make(chan bool)
+
+	rootnode = NewNode(rootpath)
+
+	go func() {
+		filepath.Walk(rootpath, addFileInfoToTree)
+		doneChan <- true
+	}()
+
+	ticker := time.NewTicker(time.Millisecond * 500)
+	go func() {
+		for _ = range ticker.C {
+			printHistogram(rootnode)
+		}
+	}()
+
+	for {
+		select {
+		case <-doneChan:
+			ticker.Stop()
+			printHistogram(rootnode)
+			return
+		}
+	}
+}
+
+func parseArgsForRootPath() string {
+	flag.Parse()
+
+	rootpath := flag.Arg(0)
+	if rootpath == "" {
+		rootpath, _ = os.Getwd()
+	}
+	rootpath, _ = filepath.Abs(rootpath)
+	if !isDir(rootpath) {
+		fmt.Printf("%s is not a directory", rootpath)
+		os.Exit(1)
+	}
+
+	return rootpath
+}
+
+func main() {
+	walkPathAndPrintResults(parseArgsForRootPath())
+}
